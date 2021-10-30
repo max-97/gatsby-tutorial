6a7426ee25b987e4db946f30afe70d2b2d3e0abc
Max Braun <maxbraun97@web.de>
2021-10-30 12:54:04 +0200

    Lesson 5

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Layout from "../components/Layout"
 
 export default function About() {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>Lore ipsum ...</p>
-            <p>Lore ipsum ...</p>
-            <p>Lore ipsum ...</p>
-        </div>
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>Lore ipsum ...</p>
+                <p>Lore ipsum ...</p>
+                <p>Lore ipsum ...</p>
+            </div>
+        </Layout>
     )
 }
