fb337f79a2fec93ef01e0915188cae5150c481cd
Max Braun <maxbraun97@web.de>
2021-10-30 13:14:13 +0200

    Lesson 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { header, btn } from '../styles/home.module.css'
+import { Link } from "gatsby"
 
 export default function Home() {
     return (
         <Layout>
-            <section>
+            <section className={ header }>
                 <div>
                     <h2>Design</h2>
                     <h3>Develop & Deploy</h3>
                     <p>UX Designer & web developer</p>
+                    <Link className={ btn } to="/project">My Portfolio Project</Link>
                 </div>
             </section>
         </Layout>
--- src/pages/project/index.js
+++ src/pages/project/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from "../../components/Layout"
+import * as styles from '../../styles/project.module.css'
 
 export default function Projects() {
     return (
         <Layout>
-            <div>
+            <div className={styles.portfolio}>
                 <h2>Portfolio</h2>
                 <h3>Projects & Website I've Created</h3>
             </div>
