feac404fb2d2b02edc417a47420828697205e5ee
Max Braun <maxbraun97@web.de>
2021-10-31 12:08:51 +0100

    Lesson 9

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { header, btn } from '../styles/home.module.css'
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
-export default function Home() {
+export default function Home({ data }) {
+    console.log(data);
+    const { title, description } = data.site.siteMetadata
     return (
         <Layout>
             <section className={ header }>
@@ -14,7 +16,19 @@ export default function Home() {
                     <Link className={ btn } to="/project">My Portfolio Project</Link>
                 </div>
                 <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }}/>
+                <p>{ title } - { description }</p>
             </section>
         </Layout>
     )
 }
+
+export const query = graphql`
+    query SiteInfo {
+        site {
+            siteMetadata {
+                description
+                title
+            }
+        }
+    }
+`
